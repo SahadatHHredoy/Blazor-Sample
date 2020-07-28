d33b3cc00d6270e107d2aed844bd7befbcd786c5
SahadatHHredoy <sahadat@encodersbd.com>
2020-07-28 13:46:52 +0600

    Initial Assignment

---
+++ BlazorApp.Web/BlazorApp.Web/Common/AlertEnum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Web.Common
+{
+    public  class AlertEnum
+    {
+        public const string Danger = "alert-danger";
+        public const string Success = "alert-success";
+    }
+}
